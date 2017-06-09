2594a81e74721ca8a6fd060e338df8ff252022e5
Akira KAWATA <akirakawata1@gmail.com>
2017-06-09 18:37:40 +0900

    Revert "Making alpha.hs."
    This reverts commit bd0f509a1e7252b78f9f463e507cef746a1e4430.

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,9 @@ import Parse
 import Eval
 import Control.Monad
 
-printResult (Right xs) = printResult' xs
-printResult (Left s) = show s
-
-printResult' [] = ""
-printResult' (Right x:xs) = show x ++ "\n" ++ printResult' xs
-printResult' (Left x:xs) = show x ++ "\n" ++ printResult' xs
-
 main :: IO ()
 main = do
   input <- getContents
+  -- putStrLn input
   print $ stringToProgram input
   print $ programToExVal `liftM` stringToProgram input
-  putStrLn $ printResult $ programToExVal `liftM` stringToProgram input
-
