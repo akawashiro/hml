1366932be4212099cfb5a28eb22659315add3a5d
akira <akirakawata1@gmail.com>
2018-07-30 21:20:10 +0900

    KNormal.

--- src/KNormal.hs
+++ src/KNormal.hs
@@ -1,62 +1,68 @@
 module KNormal where
 
-import Parse
+import qualified Parse as P
 import Control.Monad.State
 import Data.Maybe
 
--- programToKNormalProgram :: Program -> Program
--- programToKNormalProgram = map exprToKNormalExpr
+newtype Var = Var String deriving (Show)
 
-exprToKNormalExpr :: Expr -> Expr
+data Op = OLess | OPlus | OMinus | OTimes deriving (Show)
+
+data Exp = EInt Integer | 
+           EBool Bool |
+           EOp Op Exp Exp |
+           EIf Exp Exp Exp |
+           ELet Var Exp Exp |
+           EVar Var |
+           ERec Var [Var] Exp Exp |
+           EApp Exp [Exp]
+           deriving (Show)
+
+exprToKNormalExpr :: P.Exp -> Exp
 exprToKNormalExpr exp = evalState (exprToKNormalExpr' exp) 0
 
-generateNewName :: State Int String
+generateNewName :: State Int Var
 generateNewName = do
   i <- get
   put $ i + 1
-  return $ "_tmp_" ++ show i
+  return $ Var ("_tmp_" ++ show i)
 
-appExprToKNormalExpr :: Expr -> State Int Expr
-appExprToKNormalExpr exp = case exp of
-  EApp e1 e2 -> do
-    e1' <- appExprToKNormalExpr e1
-    e2' <- appExprToKNormalExpr e2
-    return $ EApp e1' e2'
-  _ -> exprToKNormalExpr' exp
-
-exprToKNormalExpr' :: Expr -> State Int Expr 
+exprToKNormalExpr' :: P.Exp -> State Int Exp
 exprToKNormalExpr' exp = case exp of
-  EInt i -> do 
-    s <- generateNewName
-    return $ ELet s (EInt i) (EVariable s)
-  -- EBool b -> return $ EBool b
-  EBinOp o e1 e2 -> do
+  P.EInt i -> return $ EInt i
+    -- s <- generateNewName
+    -- return $ ELet s (EInt i) (EVar s)
+  P.EBool True -> return $ EInt 1
+  P.EBool False -> return $ EInt 0
+  P.EOp o e1 e2 -> do
     s <- generateNewName
     e1' <- exprToKNormalExpr' e1
     e2' <- exprToKNormalExpr' e2
-    return $ ELet s (EBinOp o e1' e2') (EVariable s)
-  EIf e1 e2 e3 -> do
+    return $ ELet s (EOp (f o) e1' e2') (EVar s)
+      where f P.OLess = OLess
+            f P.OPlus = OPlus
+            f P.OMinus = OMinus
+            f P.OTimes = OTimes
+  P.EIf e1 e2 e3 -> do
     s1 <- generateNewName
     s2 <- generateNewName
     s3 <- generateNewName
     e1' <- exprToKNormalExpr' e1
     e2' <- exprToKNormalExpr' e2
     e3' <- exprToKNormalExpr' e3
-    return $ ELet s1 e1' (EIf (EVariable s1) (ELet s2 e2' (EVariable s2)) (ELet s3 e3' (EVariable s3)))
-  ELet s e1 e2 -> do
+    return $ ELet s1 e1' (EIf (EVar s1) (ELet s2 e2' (EVar s2)) (ELet s3 e3' (EVar s3)))
+  P.ELet (P.Var s) e1 e2 -> do
     e1' <- exprToKNormalExpr' e1
     e2' <- exprToKNormalExpr' e2
-    return $ ELet s e1' e2'
-  EFun s e -> do
-    e' <- exprToKNormalExpr' e
-    return $ EFun s e'
-  EApp e1 e2 -> do
+    return $ ELet (Var s) e1' e2'
+  P.EApp e1 e2 -> do
     s <- generateNewName
-    e1' <- appExprToKNormalExpr e1
-    e2' <- appExprToKNormalExpr e2
-    return $ ELet s (EApp e1' e2') (EVariable s)
-  ELetRec s1 s2 e1 e2 -> do
+    e1' <- exprToKNormalExpr' e1
+    e2' <- mapM exprToKNormalExpr' e2
+    return $ ELet s (EApp e1' e2') (EVar s)
+  P.ERec (P.Var s1) ys e1 e2 -> do
     e1' <- exprToKNormalExpr' e1
     e2' <- exprToKNormalExpr' e2
-    return $ ELetRec s1 s2 e1' e2'
-  EVariable s -> return $ EVariable s
+    return $ ERec (Var s1) (map f ys) e1' e2'
+      where f (P.Var s) = Var s
+  P.EVar (P.Var s) -> return $ EVar (Var s)
