8e96ff60c2b11fa5c7486c411500fb9a5f772045
akira <akirakawata1@gmail.com>
2018-08-08 01:42:47 +0900

    Make freevariables.

--- src/Closure.hs
+++ src/Closure.hs
@@ -1,93 +1,53 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 module Closure where
 
-import Parse
-import Control.Monad.State
-import Data.Maybe
-import qualified Data.Map as Map
+import qualified KNormal as K
+import Data.List
 
--- programToClosureProgram :: Program -> Program
--- programToClosureProgram = map exprToClosureExpr
+data Exp = EInt Integer | 
+           EOp K.Op Exp Exp |
+           EIf Exp Exp Exp |
+           ELet K.Var Exp Exp |
+           EDTuple [K.Var] Exp Exp |
+           EVar K.Var |
+           ERec K.Var [K.Var] Exp Exp |
+           EAppCls Exp [Exp] |
+           ETuple [Exp]
+           deriving (Eq)
+instance Show Exp where
+  show (EInt i) = show i
+  show (EOp o e1 e2) = "(" ++ show o ++ " " ++ show e1 ++ " " ++ show e2 ++ ")"
+  show (EIf e1 e2 e3) = "if " ++ show e1 ++ "\nthen " ++ show e2 ++ "\nelse " ++ show e3
+  show (ELet v e1 e2) = "let " ++ show v ++ " = " ++ show e1 ++ " in\n" ++ show e2
+  show (EDTuple vs e1 e2) = "let (" ++ concat (intersperse ", " (map show vs)) ++ ") = " ++ show e1 ++ " in\n" ++ show e2
+  show (EVar v) = show v
+  show (ERec x ys e1 e2) = "let rec " ++ show x ++ " " ++ show ys ++ " =\n" ++ show e1 ++ " in\n" ++ show e2
+  show (EAppCls e1 e2s) = show e1 ++ " " ++ show e2s
+  show (ETuple es) = "(" ++ concat (intersperse ", " (map show es)) ++ ")"
 
--- exp -> known variables -> free variables
-exprToFreeVariables :: Expr -> [String] -> [String]
-exprToFreeVariables exp boundedVariables = case exp of
-  EInt i -> []
-  EBool b -> []
-  EVariable v -> if v `elem` boundedVariables || take 3 v == "fun" then [] else [v]
-  EBinOp o e1 e2 ->
-    let v1 = exprToFreeVariables e1 boundedVariables in
-    let v2 = exprToFreeVariables e2 boundedVariables in
-    v1 ++ v2
-  EIf e1 e2 e3 ->
-    let v1 = exprToFreeVariables e1 boundedVariables in
-    let v2 = exprToFreeVariables e2 boundedVariables in
-    let v3 = exprToFreeVariables e3 boundedVariables in
-    v1 ++ v2 ++ v3
-  ELet s e1 e2 ->
-    let v1 = exprToFreeVariables e1 boundedVariables in
-    let v2 = exprToFreeVariables e2 (s:boundedVariables) in
-    v1 ++ v2
-  EFun s e ->
-    let v = exprToFreeVariables e (s:boundedVariables) in
-    v
-  EApp e1 e2 ->
-    let v1 = exprToFreeVariables e1 boundedVariables in
-    let v2 = exprToFreeVariables e2 boundedVariables in
-    v1 ++ v2
-  ELetRec s1 s2 e1 e2 ->
-    let v1 = exprToFreeVariables e1 (s1:s2:boundedVariables) in
-    let v2 = exprToFreeVariables e2 (s1:boundedVariables) in
-    v1 ++ v2
+adder :: K.Exp
+adder = (K.ERec (K.Var "make_adder") [(K.Var "x")]
+          (K.ERec (K.Var "adder") [(K.Var "y")] 
+            (K.EOp K.OPlus (K.EVar (K.Var "x")) (K.EVar (K.Var "y"))) (K.EApp (K.EVar (K.Var "adder")) [(K.EInt 3)]))
+          (K.EApp (K.EVar (K.Var "make_adder")) [(K.EInt 7)]))
 
-exprToClosureExpr :: Expr -> Expr
-exprToClosureExpr exp = evalState (exprToClosureExpr' exp) Map.empty
+adder2 :: K.Exp
+adder2 = (K.ERec 
+          (K.Var "make_adder") [(K.Var "x"), (K.Var "y")] 
+          (K.EOp K.OPlus (K.EVar (K.Var "x")) (K.EVar (K.Var "y"))) 
+          (K.EApp (K.EVar (K.Var "make_adder")) [(K.EInt 7), (K.EInt 10)]))
 
--- exp -> free variables in exp -> closure form exp
-exprToClosureFun :: Expr -> [String] -> Expr
-exprToClosureFun exp [] = exp
-exprToClosureFun exp (v:vs) = exprToClosureFun (EFun v exp) vs
+fv :: K.Exp -> [K.Var]
+fv (K.EInt _) = []
+fv (K.EOp _ e1 e2) = fv e1 ++ fv e2
+fv (K.EIf e1 e2 e3) = fv e1 ++ fv e2 ++ fv e3
+fv (K.ELet v e1 e2) = (fv e1 ++ fv e2) `lminus` [v]
+fv (K.EVar v) = [v]
+fv (K.ERec x ys e1 e2) = (fv e1 ++ fv e2) `lminus` (x:ys)
+fv (K.EDTuple xs e1 e2) = (fv e1 ++ fv e2) `lminus` xs
+fv (K.EApp e1 e2) = (fv e1 ++ concat (map fv e2))
+fv (K.ETuple e) = concat (map fv e)
 
-callToClosureCall :: Expr -> [String] -> Expr
-callToClosureCall exp [] = exp
-callToClosureCall exp (v:vs) = callToClosureCall (EApp exp (EVariable v)) vs
-
-exprToClosureExpr' :: Expr -> State (Map.Map String [String]) Expr
-exprToClosureExpr' exp = case exp of
-  EInt i -> return $ EInt i
-  EBool b -> return $ EBool b
-  EVariable s -> do
-    m <- get
-    case Map.lookup s m of
-      Nothing -> return (EVariable s)
-      Just vs -> return $ callToClosureCall (EVariable s) vs
-  EBinOp o e1 e2 -> do
-    e1' <- exprToClosureExpr' e1
-    e2' <- exprToClosureExpr' e2
-    return $ EBinOp o e1' e2'
-  EIf e1 e2 e3 -> do
-    e1' <- exprToClosureExpr' e1
-    e2' <- exprToClosureExpr' e2
-    e3' <- exprToClosureExpr' e3
-    return $ EIf e1' e2' e3'
-  EApp e1 e2 -> do
-    e1' <- exprToClosureExpr' e1
-    e2' <- exprToClosureExpr' e2
-    return $ EApp e1' e2'
-  EFun s e -> do
-    e' <- exprToClosureExpr' e
-    return $ EFun s e'
-  ELet s e1 e2 -> do
-    e1' <- exprToClosureExpr' e1
-    e2' <- exprToClosureExpr' e2
-    return $ ELet s e1' e2'
-  ELetRec s1 s2 e1 e2 -> do
-    m <- get
-    put $ Map.insert s1 (reverse (tail fs)) m
-    e1' <- exprToClosureExpr' (exprToClosureFun e1 (cutLast1 fs))
-    e2' <- exprToClosureExpr' e2
-    return $ ELetRec s1 (last fs) e1' e2'
-      where
-        fs = s2:filter (\x -> x /= s2 && x/=s1) (exprToFreeVariables e1 [])
-        cutLast1 [] = []
-        cutLast1 [a] = []
-        cutLast1 (v:vs) = v:cutLast1 vs
+lminus :: Eq a => [a] -> [a] -> [a]
+lminus xs ys = filter (\x -> not (elem x ys)) xs
